065a3840accd9248d138eede7b383bd16dbbb081
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-25 21:34:05 -0300

    vitin3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma IDE?',
+    'Uma IDE (Integrated Development Environment) é um software que fornece ferramentas para facilitar o desenvolvimento de programas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continente do mundo é a Ásia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos lógicos para resolver um problema ou realizar uma tarefa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz boa noite em inglês?',
+    'Boa noite em inglês é "Good night".'
+)
